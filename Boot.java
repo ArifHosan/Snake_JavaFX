ac3e2b691c97f3458094021698fdcd79418ce398
Arif Hosan <hosan.arif0@gmail.com>
2017-05-04 11:47:31 +0600

    Final Version

---
+++ Boot.java
@@ -0,0 +1,133 @@
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.Reflection;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+
+/**
+ * Created by Arif on 17-Aug-16.
+ */
+public class Boot extends Application {
+    private ArrayList<Image>startImage;
+    private Font sniper150,unchanged100,unchanged50;
+    private Scene startScene,menuScene;
+    private int cur,counter;
+    @Override
+    public void start(Stage stage) throws Exception {
+        sniper150=Font.loadFont(new FileInputStream(new File("files/start/fonts/SNIPER__.ttf")),150);
+        unchanged100=Font.loadFont(new FileInputStream(new File("files/start/fonts/unchanged.ttf")),100);
+        unchanged50=Font.loadFont(new FileInputStream(new File("files/start/fonts/unchanged.ttf")),50);
+        boot(stage);
+        stage.show();
+    }
+
+    public void boot(Stage stage) {
+        final boolean[] vis = {true};
+        Group root=new Group();
+        Canvas canvas=new Canvas(800,800);
+        startScene=new Scene(root,800,800, Color.AQUAMARINE);
+        root.getChildren().add(canvas);
+        final GraphicsContext gc=canvas.getGraphicsContext2D();
+        Reflection ref=new Reflection();
+        ref.setFraction(0.7);
+
+        Label begin=new Label("RUNNING BOY");
+        begin.setFont(sniper150);
+        begin.setLayoutX(170);
+        begin.setLayoutY(100);
+        begin.setEffect(ref);
+        root.getChildren().add(begin);
+
+        Label anyKey=new Label("Press Any Key!");
+        anyKey.setFont(unchanged100);
+        anyKey.setLayoutX(150);
+        anyKey.setLayoutY(450);
+        anyKey.setVisible(false);
+        root.getChildren().add(anyKey);
+        startImage=new ArrayList<>();
+        for(int i=0;i<9;i++)
+            startImage.add(new Image("files/start/boy/"+i+".png"));
+        cur=0; counter=0;
+        Timeline start=new Timeline(new KeyFrame(Duration.millis(100), event -> {
+            gc.clearRect(0,0,800,800);
+            begin.toBack();
+            gc.drawImage(startImage.get(cur),800-counter*10,200);
+            cur=(cur+1)%9;
+            counter++;
+        }));
+        start.setCycleCount(1);
+        start.play();
+        start.setOnFinished((event)->{
+            Timeline blink;
+            startScene.setOnKeyPressed((event1) ->{
+                mainMenu(stage);
+            });
+            blink = new Timeline(new KeyFrame(Duration.millis(300),(event2)->{
+                gc.clearRect(0,0,800,800);
+                anyKey.setVisible(vis[0]);
+                vis[0] =!vis[0];
+            }));
+            blink.setCycleCount(Timeline.INDEFINITE);
+            blink.play();
+        });
+        stage.setScene(startScene);
+    }
+
+    public void mainMenu(Stage stage) {
+        Group root=new Group();
+        menuScene=new Scene(root,800,800,Color.AZURE);
+        DropShadow shadow=new DropShadow();
+        Label newGameLabel=new Label("New Game");
+        Label settingsLabel=new Label("Settings");
+        Label hiScoreLabel=new Label("Hall of Fame");
+        Label aboutLabel=new Label("About");
+        Label exitLabel=new Label("Exit");
+        root.getChildren().addAll(newGameLabel,settingsLabel,hiScoreLabel,aboutLabel,exitLabel);
+
+        newGameLabel.setFont(unchanged50);
+        settingsLabel.setFont(unchanged50);
+        hiScoreLabel.setFont(unchanged50);
+        aboutLabel.setFont(unchanged50);
+        exitLabel.setFont(unchanged50);
+
+        newGameLabel.setEffect(shadow);
+        settingsLabel.setEffect(shadow);
+        hiScoreLabel.setEffect(shadow);
+        aboutLabel.setEffect(shadow);
+        exitLabel.setEffect(shadow);
+
+        placeLabel(newGameLabel,300,200);
+        placeLabel(settingsLabel,300,280);
+        placeLabel(hiScoreLabel,260,360);
+        placeLabel(aboutLabel,315,440);
+        placeLabel(exitLabel,330,520);
+
+        newGameLabel.setOnMouseClicked(event->{
+        });
+        //settingsLabel.setOnMouseClicked(event -> stage.setScene(settingScene));
+        //hiScoreLabel.setOnMouseClicked(event -> stage.setScene(hiScoreScene));
+        //aboutLabel.setOnMouseClicked(event -> stage.setScene(aboutScene));
+        exitLabel.setOnMouseClicked(event -> System.exit(0));
+
+        stage.setScene(menuScene);
+    }
+
+    public void placeLabel(Label label,int x,int y){
+        label.setLayoutX(x);
+        label.setLayoutY(y);
+    }
+}
---
+++ Sprite.java
@@ -0,0 +1,81 @@
+//package sample;
+
+import javafx.geometry.Rectangle2D;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
+import javax.swing.*;
+
+/**
+ * Created by Arif on 12-Aug-16.
+ */
+
+public class Sprite {
+    Image image;
+    int posX,posY;
+    double velocityX,velocityY;
+    double width,height;
+    double OFFSET_x,OFFSET_y;
+
+    Sprite(Image i) {
+        image=i;
+        width=image.getWidth();
+        height=image.getHeight();
+    }
+
+    Sprite(int x,int y) {
+        width=10;
+        height=10;
+        posX=x;
+        posY=y;
+        velocityX=0;
+        velocityY=0;
+    }
+
+    Sprite(Image i, int x,int y) {
+        image=i;
+        width=image.getWidth();
+        height=image.getHeight();
+        posX=x;
+        posY=y;
+        velocityX=0;
+        velocityY=0;
+    }
+
+    /*void setOFFSET(int x,int y) {
+        OFFSET_x=x;
+        OFFSET_y=y;
+    }*/
+
+    void setImage(String file) {
+        image=new Image(file);
+        width=image.getWidth();
+        height=image.getHeight();
+    }
+
+    void setImage(Image i) {
+        image=i;
+        width=image.getWidth();
+        height=image.getHeight();
+    }
+    Image getImage(){
+        return image;
+    }
+
+    void update(double time){
+        posX+=velocityX*time;
+        posY+=velocityY*time;
+    }
+    public void render(GraphicsContext gc) {
+        gc.drawImage(image,posX,posY);
+    }
+    /*public void render(GraphicsContext gc) {
+        gc.fillRect(posX,posY,width,height);
+    }*/
+    public Rectangle2D getBound() {
+        return new Rectangle2D(posX,posY,width,height);
+    }
+    public boolean intersects(Sprite s) {
+        return s.getBound().intersects(this.getBound());
+    }
+}
